9cd9169df55c86c81e09a451ce4c65fea95fb1e6
Dilsher <dilsherb@gmail.com>
2023-05-13 10:20:13 +0300

    Logo for navbar added.

--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Room from './room';
 
 const Navbar = () => {
   return (
     <div>
-      I am Navbar
+      <a href={<Room />}>
+        <img src="https://img.freepik.com/premium-vector/hotel-logo-simple-illustration_434503-736.jpg?w=200" alt="Logo" />
+      </a>
     </div>
   );
 };
