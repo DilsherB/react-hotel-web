3c2ae26e7920ff3469fbb59cdb41ead86cbebe0c
Dilsher <dilsherb@gmail.com>
2023-05-16 15:14:06 +0300

    button added to hero section

--- src/Hero.js
+++ src/Hero.js
@@ -3,7 +3,7 @@ import HeroText from './HeroText';
 
 const Hero = () => {
   return (
-    <div style={{ position: "relative", textAlign: "center" }}>
+    <div style={{ position: "relative" }}>
       <img
         src="https://img.freepik.com/free-photo/swimming-pool_74190-2104.jpg?size=626&ext=jpg"
         alt="hotel view"
