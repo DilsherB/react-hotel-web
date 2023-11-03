d12322ae30dc19054a3ec7fdf022d7723d3e96ca
Dilsher Balouch <dilsherb@gmail.com>
2023-11-03 14:51:30 +0300

    component for hero section created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <div className="App">
       <h1 className="hotelName">Your Hotel</h1>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Hero section</h1>
+    </div>
+  );
+};
+
+export default Hero;
