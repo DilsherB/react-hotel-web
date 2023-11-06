079db3bf0f5926b0f0d2bd5d5ecb8cf903d8e129
Dilsher Balouch <dilsherb@gmail.com>
2023-11-06 19:48:07 +0300

    hero section completed.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,9 +3,14 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="heroContainer">
-      <section className="hero">
-        <h1>You Hotel</h1>
-        <h2>Hero section</h2>
+      <h1 className="heroHeading">You Hotel</h1>
+      <section className="heroTextSection">
+        <h2 className="heroText">Welcome to U Hotel</h2>
+        <p>
+          It is all about you. We decorated it as per your taste. Because it is
+          all about you so we are always ready to customize everything as per
+          your tase.
+        </p>
       </section>
     </div>
   );
