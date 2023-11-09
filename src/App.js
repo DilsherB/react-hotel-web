baf78dad8be02c34504ecf60012c19ad3bebcd40
Dilsher Balouch <dilsherb@gmail.com>
2023-11-09 19:20:07 +0300

    Rooms component displayed on page by rendering it in App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Rooms from "./components/Rooms";
 import Hero from "./components/hero";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Rooms />
     </div>
   );
 }
